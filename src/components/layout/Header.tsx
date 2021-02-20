a2994bc1bdb44c538bb38b38308d4eb8372ce7a3
10mi8o <totoma855@gmail.com>
2021-02-20 19:09:33 +0900

    Aboutページへの導線追加

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -8,12 +8,12 @@ export const Header: FC<{ open; openFlg }> = (props): JSX.Element => {
 
   return (
     <>
-      <div className="relative bg-white mb-10">
+      <div className="relative bg-white mb-10 shadow">
         <div className="max-w-7xl mx-auto px-4 sm:px-6">
           <div className="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
             <div className="flex justify-start lg:w-0 lg:flex-1">
               <a href="/">
-                <img className="h-8 w-auto sm:h-10" src="https://test.com" alt="logo" />
+                <img className="h-8 w-auto sm:h-14" src="/logo.png" alt="logo" />
               </a>
             </div>
             <div className="-mr-2 -my-2 md:hidden">
@@ -44,10 +44,9 @@ export const Header: FC<{ open; openFlg }> = (props): JSX.Element => {
               <Link href="/blog">
                 <a className="text-base font-medium text-gray-500 hover:text-gray-900">Blog</a>
               </Link>
-
-              <a href="#" className="text-base font-medium text-gray-500 hover:text-gray-900">
-                About
-              </a>
+              <Link href="/about">
+                <a className="text-base font-medium text-gray-500 hover:text-gray-900">About</a>
+              </Link>
             </nav>
           </div>
         </div>
