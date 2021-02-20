b2c13655ebf6215a3e6217c9cca2b64bb1d69d43
10mi8o <totoma855@gmail.com>
2021-02-20 19:12:47 +0900

    blog一覧にsysPropsを渡す,DivideLineをimport

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,16 +1,20 @@
 import { fetchAllPosts } from '../../api/blog';
 import { ArticleIndexCard } from '../../components/cards/ArticleIndexCard';
+import { DivideLine } from 'components/layout/DivideLine';
+
 import Layout from 'components/layout/Index';
 import { Article } from 'types/blog';
 
 //記事一覧ページ
 export default function ArticleIndex({ contents }: { contents: Article[] }) {
+  console.log(contents);
   return (
     <Layout>
+      <DivideLine pageName="Blog" />
       <ul>
         {contents.map((content, id) => (
           <li key={id}>
-            <ArticleIndexCard fields={content.fields} />
+            <ArticleIndexCard fields={content.fields} sys={content.sys} />
           </li>
         ))}
       </ul>
