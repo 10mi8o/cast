5b25ca90f87e0c5df618eaeafa17750aee2e41a3
10mi8o <totoma855@gmail.com>
2021-02-21 14:12:46 +0900

    PostListコンポーネントの微調整

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -1,15 +1,15 @@
-import React, { useState } from 'react';
+import React, { FC, useState } from 'react';
 import { format } from 'date-fns';
 import Link from 'next/link';
 import Image from 'next/image';
 import { PostItem } from 'types/blog';
 
-const PostLink: React.FC<{ item: PostItem }> = (props) => {
+const PostLink: FC<{ item: PostItem }> = (props) => {
   const { title, isoDate, link } = props.item;
   return (
     <li className="rounded hover:bg-gray-100 -m-3 p-3">
       <Link href={link}>
-        <a className="flex items-start space-x-4">
+        <a className="flex items-start items-center space-x-4" target="_blank">
           <Image src="/icon/icon_qiita.png" width="20" height="20" alt="qiita" />
           <div className="flex-auto">
             <h3 className="font-bold text-gray-900">{title}</h3>
@@ -44,10 +44,10 @@ export const PostList: React.FC<{ items: [] }> = (props) => {
       {canLoadMore ? (
         <div className="post-list-load">
           <button
-            onClick={() => setDisplayItemsCount(displayItemsCount + 6)}
+            onClick={() => setDisplayItemsCount(displayItemsCount + 7)}
             className="flex bg-blue-500 rounded-lg font-bold text-white text-center px-4 py-3 transition duration-300 ease-in-out hover:bg-blue-600 mr-6"
           >
-            OPEN
+            Load More Article
           </button>
         </div>
       ) : (
