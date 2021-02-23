0a2d1bc8961fe84103cc3b7b51730a310b46d9d0
10mi8o <totoma855@gmail.com>
2021-02-23 09:11:55 +0900

    PostLinkコンポーネントとして切り出し

---
+++ src/components/PostLink.tsx
@@ -0,0 +1,28 @@
+import React, { FC } from 'react';
+import { format } from 'date-fns';
+import Link from 'next/link';
+import Image from 'next/image';
+import { PostItem } from 'types/blog';
+
+export const PostLink: FC<{ item: PostItem }> = (props) => {
+  const { title, isoDate, link } = props.item;
+  return (
+    <li className="rounded hover:bg-gray-100 -m-3 p-3">
+      <Link href={link}>
+        <a className="flex items-start items-center space-x-4" target="_blank">
+          <div className="w-1/12">
+            <Image src="/icon/icon_qiita.png" width="20" height="20" alt="qiita" />
+          </div>
+          <div className="flex-auto w-11/12">
+            <h3 className="font-bold text-gray-900">{title}</h3>
+            <time dateTime={isoDate} className="text-sm text-gray-500">
+              {isoDate && format(new Date(isoDate), 'yyyy/MM/dd')}
+            </time>
+            {/* TODO sourceType(qiitaやZennでの出しわけ検討)  */}
+            {/* <div>{sourceType}</div> */}
+          </div>
+        </a>
+      </Link>
+    </li>
+  );
+};
