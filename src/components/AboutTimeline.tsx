f78186ecd726caf682a08d1c6f56020678d2f1ed
10mi8o <totoma855@gmail.com>
2021-02-28 08:57:01 +0900

    pageLayoutの微調整

--- src/components/AboutTimeline.tsx
+++ src/components/AboutTimeline.tsx
@@ -14,10 +14,10 @@ export const AboutTimeLine: FC<Props> = (props) => {
     <>
       <li>
         <div className="grid md:grid-cols-7 xl:grid-cols-8 items-start relative rounded-xl p-3 sm:p-5 xl:p-6 overflow-hidden">
-          <div className="font-semibold text-gray-900 md:col-start-3 md:col-span-6 xl:col-start-3 xl:col-span-7 mb-1 ml-9 md:ml-0">
+          <div className="font-semibold md:col-start-3 md:col-span-6 xl:col-start-3 xl:col-span-7 mb-1 ml-9 md:ml-0">
             {head}
           </div>
-          <time className="md:col-start-1 md:col-span-2 row-start-1 md:row-end-3 flex items-center font-medium mb-1 md:mb-0 text-gray-500">
+          <time className="md:col-start-1 md:col-span-2 row-start-1 md:row-end-3 flex items-center font-medium mb-1 md:mb-0">
             {isCurrent ? (
               <svg viewBox="0 0 12 12" className="w-3 h-3 mr-6 overflow-visible">
                 <circle cx="6" cy="6" r="6" fill="#e89363"></circle>
@@ -36,9 +36,7 @@ export const AboutTimeLine: FC<Props> = (props) => {
             )}
             {date}
           </time>
-          <p className="text-sm md:col-start-3 md:col-span-6 xl:col-span-7 ml-9 md:ml-0  text-gray-500">
-            {body}
-          </p>
+          <p className="text-sm md:col-start-3 md:col-span-6 xl:col-span-7 ml-9 md:ml-0">{body}</p>
         </div>
       </li>
     </>
--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const Footer = (): JSX.Element => {
   return (
-    <footer className="text-center mt-10 mb-5">
+    <footer className="text-center mt-10 pb-5">
       <small>© {new Date().getFullYear()} CUE:</small>
     </footer>
   );
