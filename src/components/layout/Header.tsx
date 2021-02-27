cb35919628da039d7bef4648a879ae77137b33ba
10mi8o <totoma855@gmail.com>
2021-02-27 10:49:26 +0900

    logoのheightのプロパティをautoから数値へ変更

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -14,7 +14,7 @@ export const Header: FC<{ open; openFlg }> = (props): JSX.Element => {
           <div className="w-11/12 m-auto flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
             <div className="flex justify-start lg:w-0 lg:flex-1">
               <a href="/">
-                <SiteLogoIcon width="60" height="auto" />
+                <SiteLogoIcon width="60" height="100%" />
               </a>
             </div>
             <div className="-mr-2 -my-2 md:hidden">
